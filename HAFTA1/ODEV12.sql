2c46ffb3c0fac7a554f4c81d8598c34e247d4183
Hatice Durmus <59876821+haticedurmus@users.noreply.github.com>
2022-03-11 16:45:15 +0300

    Update ODEV12.sql

--- HAFTA1/ODEV12.sql
+++ HAFTA1/ODEV12.sql
@@ -3,3 +3,13 @@
   
 2)SElECT COUNT(*) FROM film WHERE rental_rate = 
   (SELECT MAX(rental_rate) FROM film)
+  
+3)(SELECT * FROM film ORDER BY rental_rate LIMIT 5)
+UNION
+(SELECT * FROM film ORDER BY replacement_cost LIMIT 5)
+
+4)SELECT customer_id, SUM(amount) AS customer_alisveris
+FROM payment 
+GROUP BY customer_id 
+ORDER BY SUM(amount) DESC 
+LIMIT 10
