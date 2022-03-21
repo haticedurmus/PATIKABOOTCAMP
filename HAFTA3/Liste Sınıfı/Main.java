90e85bc97b3ea004803d528f81dd4ca708e5369c
Hatice Durmus <59876821+haticedurmus@users.noreply.github.com>
2022-03-21 21:08:20 +0300

    Update Main.java

--- HAFTA3/Liste Sınıfı/Main.java
+++ HAFTA3/Liste Sınıfı/Main.java
@@ -1,19 +1,18 @@
 package ListClass;
 
-
 public class Main {
     public static void main(String[] args){
 
         MyList<Integer> liste = new MyList<>();
         System.out.println("Liste Durumu : " + (liste.isEmpty() ? "Boş" : "Dolu"));
         liste.add(10);
-        liste.add(20);
+        liste.add(80);
         liste.add(30);
-        liste.add(40);
+        liste.add(70);
         liste.add(20);
         liste.add(50);
         liste.add(60);
-        liste.add(70);
+        liste.add(25);
 
         System.out.println("Liste Durumu : " + (liste.isEmpty() ? "Boş" : "Dolu"));
 
@@ -30,12 +29,12 @@ public class Main {
         Object[] dizi = liste.toArray();
         System.out.println("Object dizisinin ilk elemanı :" + dizi[0]);
 
-        // Liste veri türünde alt bir liste oluşturdu
+        // Liste veri türünde alt bir liste oluşturur
         MyList<Integer> altListem = liste.subList(0, 5);
         System.out.println(altListem.toString());
 
 
-        // Değerim listedeki olup olmadığını sorguladı
+        // Değerin listede olup olmadığını sorgular
         System.out.println("Listemde 20 değeri : " + liste.contains(20));
         System.out.println("Listemde 120 değeri : " + liste.contains(120));
 
