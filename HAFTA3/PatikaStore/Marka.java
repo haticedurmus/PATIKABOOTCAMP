f38860b11bd377d52bcdabd1fa90e33b2a1c628e
Hatice Durmus <59876821+haticedurmus@users.noreply.github.com>
2022-03-21 20:49:41 +0300

    Add files via upload

---
+++ HAFTA3/PatikaStore/Marka.java
@@ -0,0 +1,24 @@
+package com.company;
+
+public class Marka {
+
+    int id;
+    String name;
+
+    public Marka(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Marka(){}
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
+
