3f45d1b647dd8ec4ecda87b9f08a644c877f601e
Hatice Durmus <59876821+haticedurmus@users.noreply.github.com>
2022-03-28 23:13:44 +0300

    Add files via upload

---
+++ HAFTA4/Patika Klonu/Patika.Dev/src/com/patikadev/Helper/Config.java
@@ -0,0 +1,9 @@
+package com.patikadev.Helper;
+
+public class Config {
+    public static final String PROJECT_TITLE="Patika.dev";
+    public static final String DB_URL="jdbc:postgresql://localhost/patika";
+    public static final String DB_USERNAME = "master";
+    public static final String DB_PASSWORD = "postgres";
+}
+
